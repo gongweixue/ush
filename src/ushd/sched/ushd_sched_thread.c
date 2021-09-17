bb95c2590c75df07f1867f13109a257652ca9619
gongweixue <gongweixue@faw.com.cn>
2021-09-17 17:34:07 +0800

    mock for the sched thread, pick data from the fifo

--- src/ushd/sched/ushd_sched_thread.c
+++ src/ushd/sched/ushd_sched_thread.c
@@ -1,12 +1,30 @@
 #include "ush_comm_touch.h"
 
+#include "ush_log.h"
+#include "ush_time.h"
+
 #include "ushd_sched_thread.h"
 #include "ushd_sched_fifo.h"
+
+#ifndef USH_SCHED_FIFO_LEN
+#define USH_SCHED_FIFO_LEN    (10)
+#endif
 
 ush_ret_t
 ushd_sched_thread_start() {
     // first init sched fifo for pass msg from touch thread
     ushd_sched_fifo_init(USH_SCHED_FIFO_LEN, USH_COMM_TOUCH_Q_MSG_MAX_LEN);
+
+    while(1) {
+        ush_char_t *pbuf = ushd_sched_fifo_retain(USHD_SCHED_FIFO_FULL);
+
+        // TODO: convert to basic struct of touch msg,
+        //       and branch to the worker or create a publisher thread
+        ush_time_delay_ms(500);
+        ushd_log(LOG_LVL_INFO, "delay 500 ms, seems like it do the real work");
+
+        ushd_sched_fifo_release(pbuf, USHD_SCHED_FIFO_EMPTY);
+    }
     return 0;
 }
 
